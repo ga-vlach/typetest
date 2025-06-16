5c9cdf0dd3ad8200e33ec4e1550d164ee3c6d38a
Giorgos Alexandros Vlachopoulos <alex@vlach.gr>
2025-06-16 19:55:21 +0300

    Changed code

--- typetest.py
+++ typetest.py
@@ -1,85 +1,93 @@
 import random 
 import time
-import sys
 
 
 def typing():
 
-    with open("words.txt", 'r') as file:
+    while True:
 
-        l = list(range(100))
-        random.shuffle(l)
-
-        data = file.read()
-        words = data.split()
-
-        text = ''
-        text_list = []
-
-        while True:
+        with open("words.txt", 'r') as file:
             
-            try:
-                count = int(input("How many words do you want to type?\n"))
+                l = list(range(100))
+                random.shuffle(l)
 
-            except:
-                print("\nEnter an integer please")
-            
-            else:
-                break
-        
-        j = 0
-        #Choosing random words from the document
-        for i in range(count):
+                data = file.read()
 
-            chosen_word = l[j]
+                words = data.split()
 
-            text += words[chosen_word] + ' '
-            text_list.append(words[chosen_word])
-            j+=1
+                text = ''
+                text_list = []
 
-        text.split()
-        
-        print(text)
-        
-        start_time = time.time()
-        
-        correct_words = []
-        i = 0
-        j = 0
-        
-        #Taking the correct input
+                while True:
+                    
+                    try:
+                        count = int(input("How many words do you want to type?\n"))
 
-        for line in sys.stdin:
-            #Clears every input
-            print("\033[A                             \033[A")
+                    except:
+                        print("\nEnter an integer please")
+                    
+                    else:
+                        break
+                
+                j = 0
+                #Choosing random words from the document
+                for i in range(count):
 
-            correct_words.append(line.rstrip())
-            
-            if text_list[i] != correct_words[j]:
-                correct_words.pop(j)
-            
-            else:
-                j+=1
+                    chosen_word = l[j]
 
-            if i == len(text_list) - 1:
-                break
-            i+=1
+                    text += words[chosen_word] + ' '
+                    text_list.append(words[chosen_word])
+                    j+=1
+
+                input("Press enter to start typing whenever you're ready.\n")
+                
+                text.split()
+                print(f"{text}\n")
+                
+
+                start_time = time.time()
+                
+                user = input()
+                user = list(user.split())
+                
+                j = 0
+                for i in range(len(user)):
+                    
+                    if text_list[i] != user[j]:
+
+                        user.pop(j)
+
+                    else:
+                        j+=1
+
+                end_time = time.time()
+
+                #Calculating the gross wpm
+                g_typing_speed = len(text_list) / ((end_time - start_time) / 60)
+
+                #Calculating net wpm, user is the correct words only
+                n_typing_speed = len(user) / ((end_time - start_time) / 60)
+
+                #Calculating the accuracy
+                accuracy = (n_typing_speed * 100) / g_typing_speed
+
+                print(f"Your net typing speed is {n_typing_speed:.2f} words per minute!")
+                print(f"\nYour raw typing speed is {g_typing_speed:.2f} words per minute!\n")
+                print(f"{accuracy:.2f}% accuracy")
 
 
-        end_time = time.time()
+                while True:
 
-        #Calculating the gross wpm
-        g_typing_speed = len(text_list) / ((end_time - start_time) / 60)
+                    again = str(input("\n\nDo you want to play again? "))
 
-        #Calculating net wpm
-        n_typing_speed = len(correct_words) / ((end_time - start_time) / 60)
-
-        #Calculating the accuracy
-        accuracy = (n_typing_speed * 100) / g_typing_speed
-
-        print(f"Your net typing speed is {n_typing_speed:.2f} words per minute!")
-        print(f"\nYour raw typing speed is {g_typing_speed:.2f} words per minute!\n")
-        print(f"{accuracy:.2f}% accuracy")
+                    again = again.lower()
+                
+                    if again == "no":
+                        return 0
+                    elif again == "yes":
+                        break
+                    else:
+                        print("\nInvalid answer")
 
 
 typing()
